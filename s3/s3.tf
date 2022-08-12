096d11e62ec546edfe83ea6cc1ca353ab6f9c01d
Kinshuk Garella <12112545+kinshukgarella@users.noreply.github.com>
2022-08-12 13:33:02 +0530

    Only Comment edits

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,7 +1,9 @@
-#provider "aws" {
-#  region = var.web_region
-#}
+/*
+S3 Bucket Creation Code 
+In case, S3 bucket already exist then do not use this code.
+Apply the existing S3 bucket name in belo files. 
 
+*/
 resource "aws_s3_bucket" "s3kgbucket1212" {
   bucket = var.s3_bucket
   versioning {

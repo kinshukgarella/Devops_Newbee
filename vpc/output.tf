9a09be2c63168a427646c557346af4aaa09521ef
Kinshuk Garella <12112545+kinshukgarella@users.noreply.github.com>
2022-08-30 15:35:54 +0530

    EKS on Public

--- vpc/output.tf
+++ vpc/output.tf
@@ -5,3 +5,11 @@ output "private1_subnet_id" {
 output "private2_subnet_id" {
     value = "${aws_subnet.private_2.id}"
 }
+
+output "public1_subnet_id" {
+    value = "${aws_subnet.public_1.id}"
+}
+
+output "public2_subnet_id" {
+    value = "${aws_subnet.public_2.id}"
+}
